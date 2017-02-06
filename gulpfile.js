b4162ed086315e61e530211d649a381a9f94e6d8
nathanjessen <nathan.jessen@gmail.com>
2017-02-06 09:18:27 -0800

    Move build tasks and dependencies to separate repository

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,25 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const runSequence = require('run-sequence');
+var dagger = require('dagger-tasks');
 
-const documentation = require('./tasks/documentation');
-const sass_development = require('./tasks/sass_development');
-const sass_lint = require('./tasks/sass_lint');
-const sass_production = require('./tasks/sass_production');
+var settings = {
+  browsersync: {
+    files: ['./dist/docs/**'],
+    source: './dist/docs',
+    destination: [
+      '**/*.scss',
+      '!bower_components/**/*.scss',
+      '!node_modules/**/*.scss'
+    ]
+  },
+  sass_production: {
+    browsers: ['last 2 versions', '> 5%', 'not ie < 11'],
+    source: [
+      '**/*.scss',
+      '!**/_*.scss',
+      '!bower_components/**/*.scss',
+      '!node_modules/**/*.scss'
+    ],
+    destination: './dist'
+  }
+};
 
-const default_settings = require('./config');
-const user_settings = {};
-const settings = Object.assign({}, default_settings, user_settings);
-
-gulp.task('documentation', function() {
-  return documentation(settings.documentation);
-});
-
-gulp.task('sass:lint', function() {
-  return sass_lint(settings.sass_lint);
-});
-
-gulp.task('sass:development', ['sass:lint'], function() {
-  return sass_development(settings.sass_development);
-});
-
-gulp.task('sass:production', ['sass:lint'], function() {
-  return sass_production(settings.sass_production);
-});
-
-gulp.task('build:production', function(callback) {
-  runSequence('sass:development', 'documentation', 'sass:production', callback);
-});
-
-gulp.task('watch', ['documentation'], function() {
-  browserSync.init({
-    files: settings.browsersync.files,
-    port: 4000,
-    server: {
-      baseDir: settings.browsersync.source
-    }
-  });
-
-  gulp.watch(settings.browsersync.destination, ['sass:development', 'documentation']);
-});
-
-gulp.task('default', ['watch']);
+dagger(settings);
